25bb24c9de85f59bad9465f6a4b99839c909f1c1
drohit02 <drohit045@gmail.com>
2024-12-09 15:51:46 +0530

    updated greeting code

--- greeting.py
+++ greeting.py
@@ -1,9 +1,9 @@
 name = input(str("What is your name : "))
-print("Hello,",name)
+print("Hello,",name,end=" ")
 
 """
 Representation of the print function in python
-print(*object,sep='',file=sys.stdout,flush=False)
+print(*object,sep='',end="\n",file=sys.stdout,flush=False)
 """
 
 print("Good morning !!!")
